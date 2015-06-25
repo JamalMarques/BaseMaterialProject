468e52899d6f36738e2c8a2c2c817c7c2e1c3183
JamalMarques <yamamarques@gmail.com>
2015-06-25 16:35:08 -0300

    added

--- app/src/main/java/com/jamal/materialtestproject/Activities/DeviceDetailActivity.java
+++ app/src/main/java/com/jamal/materialtestproject/Activities/DeviceDetailActivity.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,6 +25,7 @@ import java.lang.reflect.Type;
 
 public class DeviceDetailActivity extends MasterActivity {
 
+    private Toolbar toolbar;
     private SmartPhone device;
     private Button button1,buttonTouch;
 
@@ -31,6 +33,10 @@ public class DeviceDetailActivity extends MasterActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_detail);
+
+        toolbar = (Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if(getIntent().getExtras() != null) {
             if(getIntent().getExtras().getString(Constants.JSON_DEVICE) != null) {
@@ -91,21 +97,17 @@ public class DeviceDetailActivity extends MasterActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_device_detail, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id){
+            case android.R.id.home:
+                finish();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
